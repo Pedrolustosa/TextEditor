add7430046e5bade92fe4fb623209d9d7bdec15e
Pedrolustosa <pedroeternalss@gmail.com>
2021-02-28 17:38:59 -0300

    TextEditor - Menu

--- Program.cs
+++ Program.cs
@@ -8,5 +8,33 @@ namespace TextEditor
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do ?");
+            Console.WriteLine("1 - Open files");
+            Console.WriteLine("2 - Create new file");
+            Console.WriteLine("0 - Exit");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Open()
+        {
+
+        }
+        static void Edit()
+        {
+
+        }
     }
 }
